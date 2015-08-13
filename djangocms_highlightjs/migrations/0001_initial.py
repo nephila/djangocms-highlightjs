87ea98917b482e790ded9e765855e42f64d09bc8
Iacopo Spalletti <i.spalletti@nephila.it>
2015-08-13 07:21:11 +0200

    Update migration

--- djangocms_highlightjs/migrations/0001_initial.py
+++ djangocms_highlightjs/migrations/0001_initial.py
@@ -7,7 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('cms', '0012_auto_20150607_2207'),
+        ('cms', '__latest__'),
     ]
 
     operations = [
